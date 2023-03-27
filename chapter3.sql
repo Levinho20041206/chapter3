8bb92b6af6f8b79baf696501cd74d579e39c1d66
L E V I N H O <123866135+Levinho20041206@users.noreply.github.com>
2023-03-27 14:21:58 +0500

    Add files via upload

---
+++ chapter3.sql
@@ -0,0 +1,9 @@
+-- SELECT avg(salary) as avg_salary
+-- FROM instructor
+
+-- SELECT building
+-- FROM department group by building
+
+-- SELECT count(credits > 50) as count_credits
+-- FROM course
+-- WHERE dept_name = 'Comp. Sci'
